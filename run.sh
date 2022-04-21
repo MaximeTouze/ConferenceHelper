86b9cde4aea4d06ee038e56ea609e016bf687946
root <root@iaas.univ-nantes.fr>
2022-04-21 15:09:28 +0200

    Gitignore and run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+cd /var/www/multilingOEG22/;
+echo `pwd`; 
 source env/bin/activate
+pip list > pip-env.log
 set FLASK_APP=app.py;
-flask run -p 88;
+echo $FLASK_APP;
+flask run -p 443 2>error-run.log;
